b4fd5aac91daddcc14669d266d24a553026da37e
Yaroslav Trofimov <159535808+CoR0NaExTrA@users.noreply.github.com>
2026-04-23 16:23:27 +0300

    Лабораторная работа №7

---
+++ Lab7PP/Lab7PP/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static async Task Main()
+    {
+        Console.Write( "Введите путь к файлу: " );
+        string path = Console.ReadLine();
+
+        if ( !File.Exists( path ) )
+        {
+            Console.WriteLine( "Файл не найден!" );
+            return;
+        }
+
+        Console.Write( "Введите символы для удаления (например: !?,.): " );
+        string charsToRemove = Console.ReadLine();
+
+        string text = await File.ReadAllTextAsync( path );
+
+        // удаляем символы
+        foreach ( char c in charsToRemove )
+            text = text.Replace( c.ToString(), "" );
+
+        // сохраняем асинхронно
+        await File.WriteAllTextAsync( path, text );
+
+        Console.WriteLine( "Готово! Файл сохранён." );
+    }
+}
